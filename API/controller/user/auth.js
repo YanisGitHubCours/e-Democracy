39edc274955629bbac37a7b3ddafc065ffb12b8c
Yanis <ytagri1@myges.fr>
2022-06-12 23:04:27 +0200

    modify auth user for user login

--- API/controller/user/auth.js
+++ API/controller/user/auth.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt')
 const UserModel = require('../../model/user.js')
+const RoleModel = require('../../model/role.js')
 const jwt = require("jsonwebtoken");
 const Mongoose = require('mongoose');
 
@@ -31,8 +32,10 @@ const login = async(body,res) => {
     }
     // Find if user exist
     const user = await UserModel.findOne({ email });
+    const RoleUser = await RoleModel.findOne({_id: user.fk_role})
+    console.log(RoleUser)
     //if my user exist and the password match
-    if (user && (await bcrypt.compare(password, user.password)) && user.fk_role == "user") {
+    if (user && (await bcrypt.compare(password, user.password)) && RoleUser.name == "user") {
       // Create token
       const token = jwt.sign(
         { user_id: user._id, email },
